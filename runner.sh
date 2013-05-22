1c7122a7186ea1bb1615f5c459a2f14559f1dc3d
tomykaira <tomykaira@gmail.com>
2013-05-22 15:37:34 +0900

    Update runner for test

--- runner.sh
+++ runner.sh
@@ -1,12 +1,22 @@
-#!/bin/bash
+#!/bin/bash -x
 #PBS -j oe
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=2:ppn=8
 
 
 cd ${PBS_O_WORKDIR}
 
 exec > output.$PBS_JOBID 2>&1
 
-cat ${PBS_NODEFILE}
-./memcached -vv
+server=`hostname`
+started=n
+
+for host in `cat ${PBS_NODEFILE}`; do
+        if [ $host = $me -a $started = n ]; then
+                echo "It's me"
+                ./memcached &
+                started=y
+        else
+                rsh $host "cd ${PBS_O_WORKDIR}/client; ./mcb -c set -a $server -t 8 -n 10000 -l 500"
+        fi
+done
 exit 0
--- upload.sh
+++ upload.sh
@@ -2,4 +2,4 @@
 
 rsync -a . csc:memcached
 # ssh csc 'cd memcached; ./autogen.sh; ./configure --with-libevent=/home/tomykaira/libevent; make; qsub runner.sh'
-ssh csc 'cd memcached; qsub runner.sh'
+ssh csc 'cd memcached; make; cd client make; qsub runner.sh'
